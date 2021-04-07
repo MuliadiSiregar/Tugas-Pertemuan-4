ada0ec36da436c810770328e8e2d08f6679086ec
MuliadiSiregar <reygard03@gmail.com>
2021-04-07 12:00:27 +0700

    test

---
+++ app/src/main/java/com/example/fragmenappsmuliadisiregar/FirstFragment.java
@@ -0,0 +1,6 @@
+package com.example.fragmenappsmuliadisiregar;
+
+import android.app.Fragment;
+
+public class FirstFragment extends Fragment {
+}
---
+++ app/src/main/java/com/example/fragmenappsmuliadisiregar/SecondActifity.java
@@ -0,0 +1,6 @@
+package com.example.fragmenappsmuliadisiregar;
+
+import android.app.Activity;
+
+public class SecondActifity extends Activity {
+}
